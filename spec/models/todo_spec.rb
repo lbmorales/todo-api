81c9447b4a5e8472107f16da04b8a6a6c46d867c
Carlos Castellanos Vera <ccverak@gmail.com>
2017-04-05 18:03:43 +0200

    Add association/validations to models

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Todo, type: :model do
   # Association test
   # ensure Todo model has a 1:m relationship with the Item model
-  it {sould have_many(:items).dependent(:destroy)}
+  it {should have_many(:items).dependent(:destroy)}
   # Validation tests
   # ensure columns title and created_by are present before saving
   it { should validate_presence_of(:title) }
