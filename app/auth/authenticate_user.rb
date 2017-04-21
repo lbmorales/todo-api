59c0ee4d58d913067f936ce2c03f1d6d64f6f8d3
Lilibet Borges <lilibet.borges@gmail.com>
2017-04-21 18:41:04 +0200

    Authentication service: to create token with user_id

---
+++ app/auth/authenticate_user.rb
@@ -0,0 +1,26 @@
+class AuthenticateUser
+  # this class is responsible for authenticating user receive email and pwd
+  # checks if they are valid and then creates a token with the user id as the payload.
+  # or raise exception: invalid credentials
+  def initialize(email, password)
+    @email = email
+    @password = password
+  end
+
+  # Service entry point
+  def call
+    JsonWebtoken.encode(user_id: user.id) if user
+  end
+
+  private
+
+  attr_reader :email, :password
+
+  # Verify user credentials
+  def user
+    user = User.find_by(email: email)
+    return user if user && user.authenticate(password)
+    # Raise exception error if credentials are invalid
+    raise(ExceptionHandler::AuthenticationError, Message.invalid_credentials)
+  end
+end
